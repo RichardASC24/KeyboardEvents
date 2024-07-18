659ffc80c2e59c104b22bb049dae13bd8a3c2735
RichardASC24 <Richardwang2217@gmail.com>
2024-07-18 16:20:40 -0400

    finish keyboard_events

--- sketch.js
+++ sketch.js
@@ -1,39 +1,44 @@
 let myXPos = 100;
 let myYPos = 100;
+let marioImage;
 
 function preload(){
     marioImage = loadImage("https://upload.wikimedia.org/wikipedia/en/a/a9/MarioNSMBUDeluxe.png");
 }
-function setup() {
+
+function setup(){
     createCanvas(500,500);
     noStroke();
 
-    rectMode(CENTER);
+    rectMode(CENTER); // p5 use the x,y for rect to refer to the center point
+
 }
 
-function draw() {
+function draw(){
     background(0);
 
     fill(255,0,0);
-    //rect(myXPos, myYPos, 50, 50);
-    Image(marioImage,myXPos,myYPos,50,50);
+    //rect(myXPos, myYPos, 50,50);
+    image(marioImage, myXPos, myYPos,50,50);
 
-    // If the left arrow key is pressed, subtract 3 from the current value of myXPos
-    if(keyIsDown(LEFT_ARROW)) {
+    if(keyIsDown(LEFT_ARROW)){
         myXPos -= 3;
     }
-    // If the right arrow key is pressed, add 3 to the current value of myXPos
-    if(keyIsDown(RIGHT_ARROW)) {
+
+    if(keyIsDown(RIGHT_ARROW)){
         myXPos += 3;
     }
 
-    if(keyIsDown(UP_ARROW)) {
+    if(keyIsDown(UP_ARROW)){
         myYPos -= 3;
     }
 
-    if(keyIsDown(DOWN_ARROW)) {
-        myYpos += 3;
+    if(keyIsDown(DOWN_ARROW)){
+        myYPos += 3;
     }
+
+
+
 }
 
 
